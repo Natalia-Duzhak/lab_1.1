82f7cdab899a5ae36f0a01c375c4faf4ab790af7
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2021-02-23 14:25:16 +0300

    Add files via upload

---
+++ UnitTest1.1/UnitTest1.1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project1.1/FloatRange.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest11
+{
+	TEST_CLASS(UnitTest11)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			FloatRange z;
+			bool k =0;
+			k = z.rangeСheck(k);
+			Assert::AreEqual(k, false);
+		}
+	};
+}
